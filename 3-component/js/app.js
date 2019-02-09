920789183b5bf113d2263bfec7225a26eeb8b340
yomu85 <yomu85@gmail.com>
2019-02-09 15:41:07 +0900

    컴포넌트화 작업완료

--- 3-component/js/app.js
+++ 3-component/js/app.js
@@ -4,6 +4,8 @@ import SearchModel from './models/SearchModel.js'
 
 import FormComponent from './components/FormComponent.js'
 import ResultComponent from './components/ResultComponent.js'
+import ListComponent from './components/ListComponent.js'
+import TabComponent from './components/TabComponent.js'
 
 new Vue({
     el: '#app',
@@ -18,10 +20,12 @@ new Vue({
     },
     components: {
         'search-form': FormComponent,
-        'search-result': ResultComponent
+        'search-result': ResultComponent,
+        'list': ListComponent,
+        'tabs': TabComponent
     },
     created() {
-        this.selectedTab = this.tabs[1]
+        this.selectedTab = this.tabs[0]
         this.fetchKeyword()
         this.fetchHistory()
     },
